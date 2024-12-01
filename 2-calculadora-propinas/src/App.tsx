8799bd1f44444710a82b2d174a6c99a88417e3e3
angelf015 <angelf015.lf@gmail.com>
2024-11-30 21:18:53 -0600

    init calculadora

---
+++ 2-calculadora-propinas/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  const hello = "Hello world, for this person";
+  const finish = "This is the end";
+
+  return (
+    <>
+      <div className="file">
+        <h1>{hello}</h1>
+        <h2> Fin del hola mundo</h2>
+        <h3>{finish}</h3>
+        <p> Este en un parrafo</p>
+      </div>
+    </>
+  );
+}
+
+
+export default App;
